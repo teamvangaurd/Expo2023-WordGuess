3cecf76f00c3a7bb0d606a24836f9d06a3e6ecb4
Manojkumar M <manoj-19537@manoj-19537.csez.zohocorpin.com>
2023-12-10 14:42:10 +0530

    changes

--- netlify/login.js
+++ netlify/login.js
@@ -4,26 +4,35 @@ const fs = require('fs');
 const path = require('path');
 
 const dataPath = path.resolve(__dirname, './data.json');
+
 exports.handler = async function (event, context) {
   const { username } = JSON.parse(event.body);
 
   try {
     // Read the data from the data.json file
-    const rawData = fs.readFileSync(dataPath);
-    const jsonData = JSON.parse(rawData);
+    if (fs.existsSync(dataPath)) {
+      const rawData = fs.readFileSync(dataPath);
+      const jsonData = JSON.parse(rawData);
 
-    // Check for duplicate names in the JSON data
-    const isDuplicate = jsonData.some(user => user.name === username);
+      // Check for duplicate names in the JSON data
+      const isDuplicate = jsonData.some(user => user.name === username);
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ isDuplicate }),
-    };
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ isDuplicate }),
+      };
+    } else {
+      // Handle case where data file is missing
+      return {
+        statusCode: 404,
+        body: JSON.stringify({ error: 'Data file not found' }),
+      };
+    }
   } catch (error) {
     console.error('Error reading data:', error);
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: 'Internal Server Error',rawData}),
+      body: JSON.stringify({ error: 'Internal Server Error' }),
     };
   }
 };
